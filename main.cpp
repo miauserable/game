d5a7e95b7ae4879a72bb07a33a210f86cd3c791c
callie <rvyeinq@gmail.com>
2024-11-29 03:27:53 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,41 +3,41 @@
 #include "util.h"
 using namespace sf;
 int main() {
-  enum _STATE { m, p };
-  _STATE _GSTATE = _STATE::m;
-  RenderWindow window(VideoMode(640, 480), "hi");
-  window.setFramerateLimit(240);
+    enum _STATE { m, p };
+    _STATE _GSTATE = _STATE::m;
+    RenderWindow window(VideoMode(640, 480), "hi");
+    window.setFramerateLimit(240);
 
-  _INIT(window);
-  while (window.isOpen()) {
-    window.clear();
-    Event event;
-    while (window.pollEvent(event)) {
-      switch (event.type) {
-      case Event::Closed:
-        window.close();
-        break;
-      case Event::MouseButtonPressed:
-        if (event.mouseButton.button == Mouse::Left) {
-          if (checkCollision(window, "play")) {
-            _GSTATE = _STATE::p;
-          } else if (checkCollision(window, "quit")) {
+    _INIT(window);
+    while (window.isOpen()) {
+        window.clear();
+        Event event;
+        while (window.pollEvent(event)) {
+        switch (event.type) {
+        case Event::Closed:
             window.close();
-          } else if (checkCollision(window, "play")) {
-            _GSTATE = _STATE::m;
-          }
+            break;
+        case Event::MouseButtonPressed:
+            if (event.mouseButton.button == Mouse::Left) {
+            if (checkCollision(window, "play")) {
+                _GSTATE = _STATE::p;
+            } else if (checkCollision(window, "quit")) {
+                window.close();
+            } else if (checkCollision(window, "play")) {
+                _GSTATE = _STATE::m;
+            }
+            }
+            break;
+            }
         }
-        break;
+    
+        if (_GSTATE == _STATE::m) {
+            _RENDERMENU(window);
         }
+        if (_GSTATE == _STATE::p) {
+            _RENDERGAME(window);
+        }
+        window.display();
     }
-
-    if (_GSTATE == _STATE::m) {
-        _RENDERMENU(window);
-    }
-    if (_GSTATE == _STATE::p) {
-        _RENDERGAME(window);
-    }
-    window.display();
-  }
-  return 0;
+    return 0;
 }
